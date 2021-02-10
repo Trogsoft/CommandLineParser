8c368eb2689896084e35fbba2c38b5ebba8f7edb
Neil Bullock <neil@trogsoft.com>
2021-02-10 20:39:30 +0000

    Versioning.

--- version.ps1
+++ version.ps1
@@ -1,8 +1,13 @@
 $cd = get-date
 $sd = get-date -Hour 0 -Minute 0 -Second 0
 $ts = $cd - $sd
-$bn = ($ts.TotalSeconds % 3600)
+$bn = ($ts.TotalSeconds % 3600).toString("0000")
 $rn = $Env:BuildID
 $ver = "0.1.$rn.$bn";
+
+Write-Host "0.1";
+Write-Host $rn;
+Write-Host $bn;
+Write-Host "Version = $ver"
 
 Write-Host "##vso[task.setvariable variable=ver;isOutput=true]$($ver)"
