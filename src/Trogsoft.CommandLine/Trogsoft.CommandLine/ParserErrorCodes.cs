23c1c478e4b17437a0469e036d0f221121fe29bb
Neil Bullock <neil@trogsoft.com>
2021-12-19 15:21:12 +0000

    Error name consistency, added "defaulted to help" error code.

--- src/Trogsoft.CommandLine/Trogsoft.CommandLine/ParserErrorCodes.cs
+++ src/Trogsoft.CommandLine/Trogsoft.CommandLine/ParserErrorCodes.cs
@@ -13,7 +13,8 @@ namespace Trogsoft.CommandLine
         public static readonly int ERR_MULTIPLE_DEFAULT_VERBS = 0x807;
         public static readonly int ERR_RESOLVER_ERROR = 0x808;
         public static readonly int ERR_MULTIPLE_PARAMETER_TYPES = 0x810;
-        public static readonly int MULTIPLE_MODEL_PARAMETERS = 0x812;
+        public static readonly int ERR_MULTIPLE_MODEL_PARAMETERS = 0x812;
         public static readonly int ERR_MODEL_TYPE_HAS_PARAMETER_ATTRIBUTE = 0x814;
+        public static readonly int ERR_DEFAULTED_TO_HELP = 0x816;
     }
 }
